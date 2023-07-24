8339ba70746d13eddfe19822f9d664412b77f913
mtr3t <mtr3t@mtmail.mtsu.edu>
2023-07-24 16:17:56 -0500

    updated code

---
+++ 04_hybrid_clustering/minimize.py
@@ -0,0 +1,79 @@
+import numpy as np
+import cvxpy as cp
+
+def minimize(S, set_gamma, printer=False):
+    
+    # transpose S 
+    A = np.transpose(S)
+    if printer: print('A =')
+    if printer: print(A, '\n')
+    if printer: print('shape of A =', np.shape(A), '\n')
+        
+    # this is where you can perform a data projection
+    # this is skipped at the moment
+    # possible projections PCA, NormalProj, BernoulliProj
+        
+    # create coefficent matrix
+    coeff = np.zeros([np.shape(A)[1],np.shape(A)[1]])
+    if printer: print('coeff =')
+    if printer: print(coeff, '\n')
+    
+    if printer: print('\n************************************************' +
+                      '\n            Solve for coefficents')    
+    
+    for i in range(np.shape(A)[1]):
+        b = A[:,i]
+        # if printer: print('b =\n', b)
+
+        # gamma must be positive due to DCP rules.
+        gamma = cp.Parameter(nonneg="true") 
+        gamma.value = set_gamma
+        
+        # Construct the problem.
+        x = cp.Variable(np.shape(A)[1])
+
+        # Lasso
+        obj = cp.Minimize(gamma*cp.norm(A@x-b,2) + cp.norm(x,1))
+        
+        ##########################
+        # constraint = x[i] == 0 #
+        ##########################
+        
+        # obj = cp.Minimize(set_gamma*cp.norm(A@x-b,2) + cp.norm(x, 1))
+        # constraints = [x[i] == 0, sum(x) == 1]
+
+        # # L1-Perfect
+        # obj = Minimize(norm(x, 1))
+        # constraints = [A*x == b, x[i] == 0, sum(x) == 1]
+        
+        # # L1-Noisy
+        # obj = Minimize(norm(x, 1))
+        # constraints = [ A*x - b <= gamma, x[i] == 0, sum(x) == 1 ]
+
+        prob = cp.Problem(obj)
+        # prob = cp.Problem(obj, [constraint])
+    
+        prob.solve(solver='ECOS')
+
+        coeff[:,i] = np.transpose(x.value)
+    if printer: print('coeff =')
+    if printer: print(coeff,'\n')
+            
+    if printer: print('\n************************************************' +
+                      '\n         Done solving for coefficents\n')
+    
+    ############################################################
+    # zero the diag - this flips the Y points if commented out #
+    # coeff[range(coeff.shape[0]),range(coeff.shape[1])] = 0.0 #
+    # if printer: print('coeff =')                             #
+    # if printer: print(coeff,'\n')                            #
+    ############################################################
+    
+    ## Refine results...
+    ## Only use magnitude of the coefficients (no negative values)
+    coeff = np.abs(coeff)
+    if printer: print('abs of coeff =')
+    if printer: print(coeff, '\n')
+    
+    return coeff, A
+
