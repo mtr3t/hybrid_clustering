d5a685b18b5919f865785f49939aa7a2b0467d70
mtr3t <mtr3t@mtmail.mtsu.edu>
2024-06-22 13:23:16 -0500

    subspace linspace and run added

---
+++ run_subspace.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -l
+# FILENAME:  run.sh
+
+#SBATCH -A cis230279		# Allocation name
+#SBATCH --nodes=1         	# Total # of nodes (must be 1 for serial job)
+#SBATCH --ntasks=1        	# Total # of MPI tasks (should be 1 for serial job)
+#SBATCH --time=00:02:00    	# Total run time limit (hh:mm:ss)
+#SBATCH -J hello		# Job name
+#SBATCH -o hello.o%j      	# Name of stdout output file
+#SBATCH -e hello.e%j      	# Name of stderr error file
+#SBATCH -p shared  		# Queue (partition) name
+#SBATCH --mail-user=mtr3t@mtmail.mtsu.edu
+#SBATCH --mail-type=all   	# Send email to above address at begin and end of job
+
+# Manage processing environment, load compilers and applications.
+# module purge
+module --force purge 		# Unload all loaded modules and reset everything to original state.
+module load anaconda
+module use $HOME/privatemodules
+module load conda-env/mypackages-py3.11.7
+
+module list 			# List currently loaded modules.
+hostname 			# Print the hostname of the compute node on which this job is running.
+
+# Launch serial code
+./subspace.py $1
