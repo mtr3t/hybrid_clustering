a5eb08feba2a1e746b5986f35bd24de1ad68d6a2
mtr3t <mtr3t@mtmail.mtsu.edu>
2024-06-23 16:29:43 -0500

    updated time limit

--- run_subspace.sh
+++ run_subspace.sh
@@ -4,7 +4,7 @@
 #SBATCH -A cis230279		# Allocation name
 #SBATCH --nodes=1         	# Total # of nodes (must be 1 for serial job)
 #SBATCH --ntasks=1        	# Total # of MPI tasks (should be 1 for serial job)
-#SBATCH --time=00:02:00    	# Total run time limit (hh:mm:ss)
+#SBATCH --time=02:00:00    	# Total run time limit (hh:mm:ss)
 #SBATCH -J subspace		# Job name
 #SBATCH -o subspace/subspace.o%j      	# Name of stdout output file
 #SBATCH -e subspace/subspace.e%j      	# Name of stderr error file
