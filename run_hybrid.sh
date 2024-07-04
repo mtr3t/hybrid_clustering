0fd7e3dce51109ecaad838792a8764454227c0f9
mtr3t <mtr3t@mtmail.mtsu.edu>
2024-07-04 15:57:26 -0500

    edited ntasks

--- run_hybrid.sh
+++ run_hybrid.sh
@@ -3,7 +3,7 @@
 
 #SBATCH -A cis230279		# Allocation name
 #SBATCH --nodes=1         	# Total # of nodes (must be 1 for serial job)
-#SBATCH --ntasks=2        	# Total # of MPI tasks (should be 1 for serial job)
+#SBATCH --ntasks=1        	# Total # of MPI tasks (should be 1 for serial job)
 #SBATCH --time=00:20:00    	# Total run time limit (hh:mm:ss)
 #SBATCH -J hybrid		# Job name
 #SBATCH -o hybrid/hybrid.o%j      	# Name of stdout output file
